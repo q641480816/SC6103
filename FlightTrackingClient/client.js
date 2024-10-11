6b0c419e8748e678821fb3fd4fab371dd0fac944
LIU, JINLONG <LIUJ0152@NTU.EDU.SG>
2024-10-11 17:25:55 +0800

    add client test for new methods

--- FlightTrackingClient/client.js
+++ FlightTrackingClient/client.js
@@ -38,11 +38,13 @@ const testGetFlightInfo = () => {
     });
 }
 
-const testBookSeat = () => {
+const testBookSeat = async () => {
     const client = dgram.createSocket('udp4');
     console.log('Book Seat triggered');
-    const req = UTILS.marshalMessage({ method: properties.METHOD_KEY.BOOK_SEAT, 
-        params: ['testUser4', 'TR422', '80'] });
+    const req = UTILS.marshalMessage({
+        method: properties.METHOD_KEY.BOOK_SEAT,
+        params: ['testUser4', 'TR422', '80']
+    });
     client.send(req, properties.basePort, 'localhost', (err) => {
         if (err) {
             console.error('Error sending request:', err);
@@ -59,8 +61,10 @@ const testBookSeat = () => {
 }
 
 const testRegister = (client) => {
-    const req = UTILS.marshalMessage({ method: properties.METHOD_KEY.REGISTER_FOR_SEAT_UPDATE, 
-        params: ['localhost', properties.clientBase, 20000, 'TR422'] });
+    const req = UTILS.marshalMessage({
+        method: properties.METHOD_KEY.REGISTER_FOR_SEAT_UPDATE,
+        params: ['localhost', properties.clientBase, 20000, 'TR422']
+    });
     client.send(req, properties.basePort, 'localhost', (err) => {
         if (err) {
             console.error('Error sending request:', err);
@@ -76,32 +80,124 @@ const testRegister = (client) => {
     });
 }
 
-if (cluster.isMaster) {
-
-    const worker = cluster.fork();
-    setTimeout(testBookSeat, 5000);
-} else {
+const testBoardMeFirst = () => {
+    const userId = 'testUser';
+    const flightId = 'SQ629';
     const client = dgram.createSocket('udp4');
-
-    client.bind({ port: properties.clientBase, exclusive: true }, () => {
-        console.log(`Client bound to port ${properties.clientBase}`);
-
-        // Register for flight monitoring once the client is bound
-        // registerForFlightMonitoring();
-        testRegister(client);
+    console.log('Apply board me first triggered');
+    const req = UTILS.marshalMessage({
+        method: properties.METHOD_KEY.BOOK_SEAT,
+        params: [userId, flightId, '80']
     });
+    client.send(req, properties.basePort, 'localhost', (err) => {
+        if (err) {
+            console.error('Error sending request:', err);
+            client.close();
+        } else {
+            client.once('message', (msg) => {
+                const response = UTILS.unmarshalMessage(msg);
+                if (response.error) console.log(`Error: ${response.error}`)
+                if (response.res) {
+                    console.log(`Res: ${response.res}`)
+                    const req2 = UTILS.marshalMessage({
+                        method: properties.METHOD_KEY.APPLY_BOARD_ME_FIRST,
+                        params: ["rownnj", flightId]
+                    });
 
-    client.on('message', (msg, rinfo) => {
-        const message = UTILS.unmarshalMessage(msg);
-        console.log(message);
-        // console.log(`Update received: Flight ${update.flightIdentifier}, Seats Available: ${update.seatsAvailable}`);
+                    client.send(req2, properties.basePort, 'localhost', (err) => {
+                        if (err) {
+                            console.error('Error sending request:', err);
+                            client.close();
+                        } else {
+                            client.once('message', (msg) => {
+                                const response = UTILS.unmarshalMessage(msg);
+                                if (response.error) console.log(`Error: ${response.error}`)
+                                if (response.res) {
+                                    console.log(`Res: ${response.res}`)
+                                }
+                                client.close();
+                            });
+                        }
+                    });
+                }
+            });
+        }
     });
-
-    client.on('error', (err) => {
-        console.error(`UDP error: ${err}`);
-        client.close();
-    });
-
 }
 
+const testPreFligthOrder = () => {
+    const userId = 'testUser';
+    const flightId = 'TK692';
+    const client = dgram.createSocket('udp4');
+    console.log('Apply board me first triggered');
+    const req = UTILS.marshalMessage({
+        method: properties.METHOD_KEY.BOOK_SEAT,
+        params: [userId, flightId, '80']
+    });
+    client.send(req, properties.basePort, 'localhost', (err) => {
+        if (err) {
+            console.error('Error sending request:', err);
+            client.close();
+        } else {
+            client.once('message', (msg) => {
+                const response = UTILS.unmarshalMessage(msg);
+                if (response.error) console.log(`Error: ${response.error}`)
+                if (response.res) {
+                    console.log(`Res: ${response.res}`)
+                    const req2 = UTILS.marshalMessage({
+                        method: properties.METHOD_KEY.PRE_FLIGHT_ORDER,
+                        params: [userId, flightId, properties.CHICKEN_RICE]
+                    });
+
+                    client.send(req2, properties.basePort, 'localhost', (err) => {
+                        if (err) {
+                            console.error('Error sending request:', err);
+                            client.close();
+                        } else {
+                            client.once('message', (msg) => {
+                                const response = UTILS.unmarshalMessage(msg);
+                                if (response.error) console.log(`Error: ${response.error}`)
+                                if (response.res) {
+                                    console.log(`Res: ${response.res}`)
+                                }
+                                client.close();
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
+// if (cluster.isMaster) {
+
+//     const worker = cluster.fork();
+//     setTimeout(testBookSeat, 5000);
+// } else {
+//     const client = dgram.createSocket('udp4');
+
+//     client.bind({ port: properties.clientBase, exclusive: true }, () => {
+//         console.log(`Client bound to port ${properties.clientBase}`);
+
+//         // Register for flight monitoring once the client is bound
+//         // registerForFlightMonitoring();
+//         testRegister(client);
+//     });
+
+//     client.on('message', (msg, rinfo) => {
+//         const message = UTILS.unmarshalMessage(msg);
+//         console.log(message);
+//         // console.log(`Update received: Flight ${update.flightIdentifier}, Seats Available: ${update.seatsAvailable}`);
+//     });
+
+//     client.on('error', (err) => {
+//         console.error(`UDP error: ${err}`);
+//         client.close();
+//     });
+
+// }
+
 // testBookSeat();
+
+testPreFligthOrder()
